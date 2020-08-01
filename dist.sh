59da9ac056d2aba5922ca3af98e7025901523853
kinfkong <kinfkong@126.com>
2020-08-02 01:01:41 +0800

    update json

--- dist.sh
+++ dist.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -rf dist
 mkdir -p dist/colab-connector
-cp *.txt *.png *.js *.html dist/colab-connector
+cp *.txt *.png *.js *.html *.json dist/colab-connector
 cd dist
 zip -r colab-connector.zip colab-connector
 cd -
